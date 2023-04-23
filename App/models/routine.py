a109d7e2e2f9e5cc3cbce9a1187daa7f0490ac48
Shaq2412 <82240174+Shaq2412@users.noreply.github.com>
2023-04-23 18:05:44 -0400

    Update routine.py

--- App/models/routine.py
+++ App/models/routine.py
@@ -4,7 +4,7 @@ class Routine(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
-    title = db.Column(db.Integer, nullable = False)
+    title = db.Column(db.String, nullable = False)
     days = db.relationship('Day', backref='routine')
 
     def __init__(self, userId, title):
